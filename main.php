fe0a031d1a8d4d3e69b4434d66e0adb7d02a6155
chati112 <133596038+chati112@users.noreply.github.com>
2024-02-20 22:23:30 +0100

    Add files via upload

---
+++ main.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Animalover</title>
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script>
+    
+    </script>
+</head>
+
+<body>
+    <header>
+    <nav>
+        <ul class='nav-bar'>
+            <li class='logo'><a href='#'><img src='https://img.freepik.com/darmowe-wektory/recznie-rysowane-ilustracja-sylwetki-zwierzat_23-2149550558.jpg?w=740&t=st=1708463083~exp=1708463683~hmac=6eaa0b8d1df80f5ff011b39cfaed090d1724187045525108da2ce70945182e28'/></a></li>
+            <input type='checkbox' id='check' />
+            <span class="menu">
+                <li><a href="">Home</a></li>
+                <li><a href="">Animals</a></li>
+                <li><a href="">Shelters</a></li>
+                <li><a href="">About us</a></li>
+                <li><a href="">Contact</a></li>
+                <label for="check" class="close-menu"><i class="fas fa-times"></i></label>
+            </span>
+            <label for="check" class="open-menu"><i class="fas fa-bars"></i></label>
+        </ul>
+    </nav>
+    </header>
+</body>
+
+</html>
